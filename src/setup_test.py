9fc159088b3ee7384733bdad951d8ba3e3aebe6b
WorldOfVarghese <workistreat@gmail.com>
2026-06-04 12:58:54 +0530

    Add Python environment setup test

---
+++ src/setup_test.py
@@ -0,0 +1,11 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import sklearn
+import pandas as pd
+print("Setup works.")
+print("OpenCV version:", cv2.__version__)
+print("MediaPipe imported successfully.")
+print("NumPy version:", np.__version__)
+print("scikit-learn imported successfully.")
+print("pandas version:", pd.__version__)
